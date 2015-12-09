ae2229de03ea53474880cffb6463f0444d001492
Nur Rahmad Wahyu <nurrahmad123@gmail.com>
2015-12-09 17:14:06 +0800

    Nur Rahmad Wahyu Saputro

---
+++ Nur-Rahmad-Wahyu-S/Modul-9/proseslog.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if($_POST['username'] == 'Wanda' && $_POST['password'] == '12345678') {
+	$_SESSION['login'] = true;
+	$_SESSION['username'] = $_POST['username'];
+	echo "Login berhasil. Klik <a href = 'admin.php'>Halaman Admin</a> untuk meelihat data sensitif atau <a href = 'logout.php'>logout</a> untuk keluar.";
+}
+else {
+	echo "Kombinasi username dan password salah. Silakan <a href = 'login.php'>login</a> kembali.";
+}
+?>
