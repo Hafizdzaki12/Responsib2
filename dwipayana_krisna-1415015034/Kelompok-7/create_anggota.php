a5be687d1cbf5749301b014dc37e1cbe4573cfd2
krishna666 <dwipayanakrisna21@gmail.com>
2016-01-06 17:14:44 +0800

    Create create_anggota.php

---
+++ dwipayana_krisna-1415015034/Kelompok-7/create_anggota.php
@@ -0,0 +1,39 @@
+
+<form method="post" name="form1" action="create_anggota_proses.php">
+  <table align="center">
+    <tr valign="baseline">
+      <td nowrap align="right">username:</td>
+      <td><input type="text" name="username" value="" size="32"></td>
+    </tr>
+        <tr valign="baseline">
+      <td nowrap align="right">password:</td>
+      <td><input type="password" name="password" value="" size="32"></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap align="right">Nama:</td>
+      <td><input type="text" name="nama_lengkap" value="" size="32"></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap align="right">Alamat:</td>
+      <td><input type="text" name="alamat" value="" size="32"></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap align="right">Kota:</td>
+      <td><input type="text" name="kota" value="" size="32"></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap align="right">Email:</td>
+      <td><input type="text" name="email" value="" size="32"></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap align="right">Telepon:</td>
+      <td><input type="text" name="telepon" value="" size="32"></td>
+    </tr>
+    <tr valign="baseline">
+      <td nowrap align="right">&nbsp;</td>
+      <td><input type="submit" value="Insert record"></td>
+    </tr>
+  </table>
+  <input type="hidden" name="MM_insert" value="form1">
+</form>
+<p>&nbsp;</p>
