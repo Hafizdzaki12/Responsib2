41813902d467f832af4eac3a28d2e32ff71ac226
krishna666 <dwipayanakrisna21@gmail.com>
2016-01-06 17:20:18 +0800

    Create register.php

---
+++ dwipayana_krisna-1415015034/Kelompok-7/register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Resident Evil</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="contents">
+		<div id="tagline" class="clearfix">
+			<h1>Register</h1>
+			<div>
+				<form  method="POST" action="cekregister.php">
+						<label>Nama :</label>
+						<input type="text" name="nama" id="name" placeholder="name" required><br>
+						<label>Username :</label>
+						<input type="text" name="username" id="username" placeholder="Username" required>
+						<br>
+						<label>Password  :</label>
+						<input type="password" name="password" id="password" placeholder="Password" required>
+						<br>
+						<input type="submit" value="Register">
+ 				</form>
+ 				<br>
+			</div>
+		</div>
+	</div>
+		<div id="footer">
+		<div class="clearfix">
+			<div id="connect">
+				<a href="http://freewebsitetemplates.com/go/facebook/" target="_blank" class="facebook"></a><a href="http://freewebsitetemplates.com/go/googleplus/" target="_blank" class="googleplus"></a><a href="http://freewebsitetemplates.com/go/twitter/" target="_blank" class="twitter"></a><a href="http://www.freewebsitetemplates.com/misc/contact/" target="_blank" class="tumbler"></a>
+			</div>
+			<p>
+				© 2015 By ---. 
+			</p>
+		</div>
+	</div>
+</body>
+</html>
